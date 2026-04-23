4c2f8be5a500d65205736d4ae883c6291ff3254f
zrkk95 <zrkk95@gmail.com>
2026-04-23 02:50:24 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
- base: base:'/',
+ base: '/',
   server: {
     host: "::",
     port: 8080,
